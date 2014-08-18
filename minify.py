b8c37382de8a4fa7865dae8d0c7c39fdb0d1dd2b
Michael Spector <spektom@gmail.com>
2014-08-18 17:00:55 +0300

    Add one more service to try when minifying CSS

--- minify.py
+++ minify.py
@@ -1,11 +1,16 @@
 #!/usr/bin/env python2.7
 
-import httplib, urllib, sys, glob, os.path, re, json, datetime, socket
+import httplib, urllib, sys, glob, os.path, re, json, datetime, socket, requests
 
 HTML_EXT = (".py", ".html", ".js")
 
 def minified_name(f):
 	return re.sub("\.([^\.]*)$", ".min.\\1", f)
+
+def write_minified_file(f, data):
+	with open(minified_name(f), "w") as w:
+		w.write(data)
+	print "Written %s" % minified_name(f)
 
 # Reads response from the service, and writes the result
 # into the minified version file.
@@ -19,29 +24,36 @@ def read_response(f, conn):
 	if (len(data) < 3):
 		sys.exit("\nERROR: can't process %s" % f)
 
-	with open(minified_name(f), "w") as w:
-		w.write(data)
-	print "Written %s" % minified_name(f)
+	write_minified_file(f, data)
 
 # Minify CSS file
 def css_minify(f):
-	print "Minifying CSS %s" % f
+	content = file(f, "r").read()
 	try:
-		data = { "input" : file(f, "r").read() }
+		print "Minifying CSS %s [using cssminifier.com]" % f
+		data = { "input" : content }
 		headers = { "Content-type": "application/json" }
 		conn = httplib.HTTPConnection("cssminifier.com", timeout=3)
 		conn.request("POST", "/raw", json.dumps(data), headers)
 		read_response(f, conn)
 	except socket.timeout:
-		params = urllib.urlencode({"file1": file(f, "r").read()})
-		headers = {"Content-Type": "application/x-www-form-urlencoded"}
-		conn = httplib.HTTPConnection("reducisaurus.appspot.com", timeout=3)
-		conn.request("POST", "/css", params, headers)
-		read_response(f, conn)
+		try:
+			# Try another service:
+			print "Minifying CSS %s [using cnvyr.io]" % f
+			r = requests.post("http://srv.cnvyr.io/v1?min=css", files={"files0": content}, timeout=3)
+			write_minified_file(f, r.text.replace("/*** files0 ***/", "").strip())
+		except:
+			# Try yet another service
+			print "Minifying CSS %s [using reducisaurus.appspot.com]" % f
+			params = urllib.urlencode({"file1": content})
+			headers = {"Content-Type": "application/x-www-form-urlencoded"}
+			conn = httplib.HTTPConnection("reducisaurus.appspot.com", timeout=3)
+			conn.request("POST", "/css", params, headers)
+			read_response(f, conn)
 
 # Compile JS file
 def js_compile(f):
-	print "Compiling JS %s" % f
+	print "Compiling JS %s [using closure-compiler.appspot.com]" % f
 	params = urllib.urlencode([
 		("js_code", file(f, "r").read()),
 		("compilation_level", "SIMPLE_OPTIMIZATIONS"),
